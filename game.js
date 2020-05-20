0633addeb099845eb4424a5016d1421d5b31baeb
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-20 14:19:23 -0400

    step 5 done

--- game.js
+++ game.js
@@ -6,13 +6,17 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    playSound(randomChosenColor);
+}
 
-    $("#" + randomChosenColor).fadeIn().fadeOut(100).fadeIn(100);
-    let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+function playSound(name) {
+    $("#" + name).fadeIn().fadeOut(100).fadeIn(100);
+    let audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
 }
 
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
 });

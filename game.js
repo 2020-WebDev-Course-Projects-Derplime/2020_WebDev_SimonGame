3aa697d9108a99371e93039683602c12bdda358d
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-20 14:57:38 -0400

    steps 8 and 9

--- game.js
+++ game.js
@@ -1,12 +1,40 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
-const userClickedPattern = [];
+var gamePattern = [];
+var userClickedPattern = [];
+var gameStart = false;
+var level = 0;
+
+// Handle Game start on any key presses
+$(document).keypress(function() {
+    if (!gameStart) {
+        gameStart = true;
+        $("#level-title").text(`Level ${level}`);
+        nextSequence();
+    }
+});
+
+// Handle button clicks by user
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
 
 function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text(`Level ${level}`);
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    animatePress(randomChosenColor);
+
+    $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
 }
 
@@ -16,17 +44,29 @@ function playSound(name) {
 }
 
 function animatePress(currentColor) {
-    // I personally like this fade in and out.....
-    // $("#" + name).fadeIn().fadeOut(100).fadeIn(100);
     $(`#${currentColor}`).toggleClass("pressed");
     setTimeout(function() {
         $(`#${currentColor}`).toggleClass("pressed");
     }, 100);
 }
 
-$(".btn").click(function() {
-    let userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    animatePress(userChosenColor);
-    playSound(userChosenColor);
-});
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        let wrongAudio = new Audio("sounds/wrong.mp3");
+        wrongAudio.play();
+
+        $("body").toggleClass("game-over");
+        setTimeout(function() {
+            $("body").toggleClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+}

a60b06f20f7588c149794806af2e02c06f801d48
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-20 14:27:05 -0400

    step 6 done

--- game.js
+++ game.js
@@ -6,17 +6,27 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    animatePress(randomChosenColor);
     playSound(randomChosenColor);
 }
 
 function playSound(name) {
-    $("#" + name).fadeIn().fadeOut(100).fadeIn(100);
     let audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
+}
+
+function animatePress(currentColor) {
+    // I personally like this fade in and out.....
+    // $("#" + name).fadeIn().fadeOut(100).fadeIn(100);
+    $(`#${currentColor}`).toggleClass("pressed");
+    setTimeout(function() {
+        $(`#${currentColor}`).toggleClass("pressed");
+    }, 100);
 }
 
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
     playSound(userChosenColor);
 });
